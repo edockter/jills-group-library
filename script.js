9af5066633b61fb9f1530abe0d6a731cd8e484a1
Eric Dockter <eric.dockter@gmail.com>
2016-11-19 04:03:14 -0500

    Updates to: * Fonts * Filtering Boxes * Button functions and animations

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
 $(document).ready( function () {    
-    function UpdateAuthorListBox() {        
+    function UpdateFilterListBoxes() {        
         $.getJSON("Library.json", function(json) {         
             var data = json.data;
-            var authorList = [];
-
-            for(var i=0; i < data.length; i++) {                
-                if (Array.isArray(data[i].Authors)) {
-                    for (var j=0; j < data[i].Authors.length; j++) {
-                        authorList.push(data[i].Authors[j].toString());
-                    }
-                }
-                else {
-                    authorList.push(data[i].Authors.toString());
-                }
-            }
-           
-           // remove duplicates
-           var uniqueAuthors = authorList.filter(function(itm,i,a){
-               return i==a.indexOf(itm);
-            });
-            
-            uniqueAuthors.sort();
+            var authorList = ProcessAuthorsList(json.data);
+            var masterList = ProcessAllLists(json.data);  
 
             // Append all unique authors from our list to the selectlist
-           for (i = 0; i < uniqueAuthors.length; i++) {
-                $('.select-list').append('<option>' + uniqueAuthors[i].toString() + '</option>');
-            } 
+           for (i = 0; i < authorList.length; i++) {
+                $('#author-list').append('<option>' + authorList[i].toString() + '</option>');
+            }
+
+            // Process & append other lists too
+            for (i = 0; i < masterList[0].length; i++) {
+                $('#core-value-list').append('<option>' + masterList[0][i].toString() + '</option>');
+            }
+            for (i = 0; i < masterList[1].length; i++) {
+                $('#current-reader-list').append('<option>' + masterList[1][i].toString() + '</option>');
+            }
+            for (i = 0; i < masterList[2].length; i++) {
+                $('#status-list').append('<option>' + masterList[2][i].toString() + '</option>');
+            }            
         });
     };    
+
+    function ProcessAllLists(jsonData) {
+        var coreValueList = [];
+        var statusList = [];
+        var currentReaderList = [];
+        var returnList = [];
+
+        for(var i=0; i < jsonData.length; i++) {           
+            coreValueList.push(jsonData[i].CoreValue.toString());
+            statusList.push(jsonData[i].Status.toString());
+            currentReaderList.push(jsonData[i].CurrentReader.toString());
+        }
+           
+        // remove duplicates
+        var coreValueList = coreValueList.filter(function(itm,i,a){
+            return i==a.indexOf(itm);
+        });
+        var statusList = statusList.filter(function(itm,i,a){
+            return i==a.indexOf(itm);
+        });
+        var currentReaderList = currentReaderList.filter(function(itm,i,a){
+            return i==a.indexOf(itm);
+        });
+        
+        coreValueList.sort();
+        statusList.sort();
+        currentReaderList.sort();
+
+        returnList.push(coreValueList);
+        returnList.push(currentReaderList);
+        returnList.push(statusList);
+
+        return returnList;
+    };
+
+    function ProcessAuthorsList(jsonData) { 
+        var authorList = [];   
+        for(var i=0; i < jsonData.length; i++) {                
+            if (Array.isArray(jsonData[i].Authors)) {
+                for (var j=0; j < jsonData[i].Authors.length; j++) {
+                    authorList.push(jsonData[i].Authors[j].toString());
+                }
+            }
+            else {
+                authorList.push(jsonData[i].Authors.toString());
+            }
+        }
+           
+        // remove duplicates
+        var uniqueAuthors = authorList.filter(function(itm,i,a){
+            return i==a.indexOf(itm);
+        });
+        
+        uniqueAuthors.sort();
+
+        return uniqueAuthors;
+    };
 
     // Initialize the datatable
     $('#datatable').DataTable( {
@@ -36,16 +87,21 @@ $(document).ready( function () {
             "columns": [                 
                 { "data": "Title" },
                 { "data": "Authors" },
-                { "data": "Core Value" },
+                { "data": "CoreValue" },
                 { "data": "Status" },
-                { "data": "Current Reader"}
+                { "data": "CurrentReader"}                
             ],
 			"paging": false,
-			"searching": true                        
+            "ordering": false,
+			"searching": true,
+            "sDom": '<"top"i>rt<"bottom"lp><"clear">'                        
 		});
     
     // Update the listbox on the form for author filtering
-    UpdateAuthorListBox();
+    UpdateFilterListBoxes();
+
+    // add another column
+    $('#datatable').DataTable();
 
     // when selectlist value is changed, filter the datatable
     $('.select-list').change(function() {
@@ -57,3 +113,26 @@ $(document).ready( function () {
     });
 } );
 
+function toggleFilter($elements) {
+    $elements.toggle();
+}
+
+$('#filter-button').click(function() {
+    JelloButton('#filter-button');
+
+    $(".select-list").animate({
+        height: "toggle",
+        opacity: "toggle"
+    }, "slow");
+});
+
+$('#search-button').click(function() { JelloButton('#search-button') });
+$('#add-book-button').click(function() { JelloButton('#add-book-button') });
+
+function JelloButton(selectorString) {
+    var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';    
+    
+    $(selectorString).addClass('animated jello').one(animationend,function() {
+          $(selectorString).removeClass('animated jello');
+        });
+}
