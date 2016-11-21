d4c8ccc70e5273a741dcb2deb12c8c4823b831d3
Eric Dockter <eric.dockter@gmail.com>
2016-11-21 14:49:24 -0500

    Removed nodemon requirement from packages. Added Heroku code to server.js.

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ app.use('/css', express.static('public/css'));
 app.use('/assets', express.static('public/assets'));
 app.use('/scripts', express.static('public/scripts'));
 
-// viewed at http://localhost:8080
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
