dc4cd95fc0b8fc04e6a3beb2575cae25f5f62a19
Eric Dockter <eric.dockter@gmail.com>
2016-11-23 20:00:41 -0500

    Rearranged POST method so error output is returned on all paths. Added UPDATE and DELETE methods for /api/books/ route.  Veeeeeery repetitive right now.  Will edit these route methods down once full app is built.

--- routes/index.js
+++ routes/index.js
@@ -47,15 +47,32 @@ router.post('/api/books', (req, res, next) => {
                 var newID = result.rows[0].bookid;                
                 var authorData = data.Author;
 
+                // if we have multiple authors, create a row for each one
                 if (util.isArray(authorData)) {
                     for (var i = 0; i < authorData.length; i++) {
                         const authorQuery = client.query('INSERT INTO authors(AuthorName, bookId) values($1, $2)',
-                        [authorData[i], newID]);
+                        [authorData[i], newID], function(err, result) {
+                            if (err) {
+                                // error
+                                done();
+                                console.log(err);
+                                return res.status(500).json({success: false, data: err});
+                            }
+                        });
                     }
                 }
+
+                // if we have 1 author, create 1 row
                 else {
                     const authorQuery = client.query('INSERT INTO authors(AuthorName, bookId) values($1, $2)',
-                        [authorData, newID]);
+                        [authorData, newID], function(err, result) {
+                            if (err) {
+                                // error
+                                done();
+                                console.log(err);
+                                return res.status(500).json({success: false, data: err});
+                            }
+                        });
                 }
             }
         });
@@ -99,6 +116,143 @@ router.get('/api/books', (req, res, next) => {
       done();
       return res.json(results);
     });
+  });
+});
+
+router.put('/api/books/:bookid', (req, res, next) => {
+    const results = [];
+    const bookid = req.params.bookid;
+    // Grab data from http request
+    const data = { 
+        Title: req.body.title, 
+        CoreValue: req.body.corevalue, 
+        Status: req.body.status, 
+        CurrentReader: req.body.currentReader, 
+        Author: req.body.author };
+  
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, (err, client, done) => {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        
+        // update row in Books table, on success, update Authors
+        client.query('UPDATE books SET title=($1), corevalue=($2), status=($3), currentreader=($4) WHERE bookid=($5)',
+        [data.Title, data.CoreValue, data.Status, data.CurrentReader, bookid], function(err, result) {
+            if (err) {
+                // error
+                done();
+                console.log(err);
+                return res.status(500).json({success: false, data: err});
+            }
+            else {
+                var authorData = data.Author;
+
+                // if we have multiple authors, drop all and add again                
+                if (util.isArray(authorData)) {
+                    const bookQuery = client.query('DELETE FROM authors WHERE bookid=($1)',
+                     [bookid], function(err, result) {
+                            if (err) {
+                                // error
+                                done();
+                                console.log(err);
+                                return res.status(500).json({success: false, data: err});
+                            }
+                            else {
+                                for (var i = 0; i < authorData.length; i++) {
+                                    const authorQuery = client.query('INSERT INTO authors(AuthorName, bookId) values($1, $2)',
+                                    [authorData[i], bookid], function(err, result) {
+                                        if (err) {
+                                            // error
+                                            done();
+                                            console.log(err);
+                                            return res.status(500).json({success: false, data: err});
+                                        }
+                                    });
+                                }
+                            }
+                    });                    
+                }
+
+                // if we have 1 author, update the 1 row
+                else {
+                    const authorQuery = client.query('UPDATE authors SET authorname=($1) WHERE bookid=($2)',
+                        [authorData, bookid], function(err, result) {
+                            if (err) {
+                                // error
+                                done();
+                                console.log(err);
+                                return res.status(500).json({success: false, data: err});
+                            }
+                        });
+                }
+            }
+        });    
+
+        // SQL Query > Update Data > Concatenate all authors to a single string, return { id, title, authors, corevalue, status, currentreader }    
+        const query = client.query("SELECT authors.bookId, array_to_string(array_agg(distinct authors.authorname),', ') AS Authors, books.title, books.corevalue, books.status, books.currentreader FROM authors JOIN books on authors.bookId = books.bookId GROUP BY authors.bookId, books.bookId ORDER BY books.Title");    
+        // Stream results back one row at a time
+            query.on('row', (row) => {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+});
+
+router.delete('/api/books/:bookid', (req, res, next) => {
+  const results = [];
+  // Grab data from the URL parameters
+  const bookid = req.params.bookid;
+  // Grab data from http request
+  const data = {text: req.body.text, complete: req.body.complete};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+        // Handle connection errors
+        if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Update Data
+        const authorDeleteQuery = client.query('DELETE FROM authors WHERE bookid=($1)',
+        [bookid], function(err, result) {
+            if (err) {
+                // error
+                done();
+                console.log(err);
+                return res.status(500).json({success: false, data: err});
+            }
+            else {                
+                const bookDeleteQuery = client.query('DELETE FROM books WHERE bookid=($1)',
+                [bookid], function(err, result) {
+                    if (err) {
+                        // error
+                        done();
+                        console.log(err);
+                        return res.status(500).json({success: false, data: err});
+                    }
+                });
+            }
+        });    
+    
+        // SQL Query > Update Data > Concatenate all authors to a single string, return { id, title, authors, corevalue, status, currentreader }    
+        const query = client.query("SELECT authors.bookId, array_to_string(array_agg(distinct authors.authorname),', ') AS Authors, books.title, books.corevalue, books.status, books.currentreader FROM authors JOIN books on authors.bookId = books.bookId GROUP BY authors.bookId, books.bookId ORDER BY books.Title");    
+        // Stream results back one row at a time
+        query.on('row', (row) => {
+        results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+        done();
+        return res.json(results);
+        });
   });
 });
 
