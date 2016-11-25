564a06a8d9f08620fa9701893d4cb9b9b141769d
Eric Dockter <eric.dockter@gmail.com>
2016-11-25 03:14:24 -0500

    Added: * Modal popup w/ book details when clicking "Title" column. * AJAX PUT and DELETE attached to appropriate buttons. * Updated filter targets. * added SetOutput() method -- takes n+1 jQuery objects and sets their text/value/selected-value depending on the type. * other misc. changes.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,7 +6,7 @@ $(document).ready( function () {
             "sAjaxDataProp": "",
             "ajax": "/api/books",
             "columnDefs": [
-                { "visible": false, "targets": 0 },  
+                { "visible": false, "targets": 0 },
                 { "className": "dt-center", "targets": [ 2, 3, 4 ] }
             ],
             "columns": [
@@ -20,11 +20,17 @@ $(document).ready( function () {
 			"paging": false,            
             "ordering": false,            
 			"searching": true,
-            "sDom": '<"top"i>rt<"bottom"lp><"clear">'                        
-		});
-    
-    // Update the listbox on the form for author filtering
-    UpdateFilterListBoxes();    
+            "sDom": '<"top"i>rt<"bottom"lp><"clear">',
+            "initComplete":function(settings, json) {
+                $('tr td:first-child').click(function() { 
+                    var clickedBookId = $datatable.row(this).data().bookid;
+                    PopulateModal(clickedBookId);
+                });
+
+                // Update the listbox on the form for author filtering
+                UpdateFilterListBoxes(json);
+            } 
+		});    
     
     // when selectlist value is changed, filter the datatable
     $('.filter').change(function() {        
@@ -40,16 +46,28 @@ $(document).ready( function () {
     //
     // Click Listeners
     //
-    $('#datatable').on('click', 'tr', function () {        
-        $('#bookDetails').modal("show");
+    $('.btn-lg').click(function() {
+        AnimateSelector('#' + $(this).attr('id'), 'jello');        
+    });
+    $('#details-modal-edit-button').click(function() {         
+        AnimateSelector('#details-output', 'slideOutUp');
+        $('.details-output').fadeToggle();
+        $('.details-control').fadeToggle();
+        // $('.details-output').fadeOut(1000, function() {
+        //     AnimateSelector('.details-output', 'slideOutUp');
+        // });
+
+        // $('.details-control').fadeIn(1000, function() {
+        //     AnimateSelector('.details-control', 'slideInUp');
+        // });
+        
+        
+        $('#details-modal-save-button').fadeToggle();        
+        AnimateSelector('#details-modal-save-button', 'slideInUp');
+        
     });
     
-    $('#search-button').click(function() { AnimateSelector('#search-button', 'jello') });
-    $('#add-book-button').click(function() { AnimateSelector('#add-book-button', 'jello') });
-
     $('#filter-button').click(function() {
-        AnimateSelector('#filter-button', 'jello');
-
         $(".filter").animate({
             height: "toggle",
             opacity: "toggle"
@@ -87,45 +105,30 @@ $(document).ready( function () {
     });
 
     $('.filter.author-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {        
-        FilterSearchDatatable($datatable, event, 1);
-    });
-
-    $('.filter.core-value-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {        
         FilterSearchDatatable($datatable, event, 2);
     });
 
-    $('.filter.status-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {
+    $('.filter.core-value-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {        
         FilterSearchDatatable($datatable, event, 3);
     });
 
-    $('.filter.current-reader-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {
+    $('.filter.status-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {
         FilterSearchDatatable($datatable, event, 4);
+    });
+
+    $('.filter.current-reader-list').on('changed.bs.select', function(event, clickedIndex, newValue, oldValue) {
+        FilterSearchDatatable($datatable, event, 5);
     });    
     
     $('#addbook-modal-save-button').click(function(event) {
         // prevent form submission
         event.preventDefault();
-        var $form = $('#addBooksForm');
-        var inputValues = {};
+        var $form = $('#addBooksForm');        
         var ajaxURL = $form.attr('action');
-        var ajaxData = $form.serializeArray();
-        var serialize = $form.serialize();        
+        var ajaxData = $form.serializeArray();        
         var ajaxPost = $.post( ajaxURL, ajaxData );
-
-        var formTitle = ajaxData[0].value;
-        var formAuthors = ajaxData[1].value;
-        var formCoreValue = ajaxData[2].value;
-        var formStatus = ajaxData[3].value;        
-        
-        ajaxPost.done(function( data ) {                      
-            // $datatable.row.add({
-            // Title: formTitle,
-            // Authors: formAuthors,
-            // CoreValue: formCoreValue,            
-            // Status: formStatus,
-            // CurrentReader: ''
-            // }).draw( false );
-
+       
+        ajaxPost.done(function( data ) {
             $datatable.ajax.reload();
 
             swal("Book saved!", "Book has been added to the library.", "success");
@@ -134,7 +137,74 @@ $(document).ready( function () {
         ajaxPost.fail(function() {
             swal("Save error.", "There was an error submitting the form.", "error");
         });    
-     });    
+     });
+
+     $('#details-modal-save-button').click(function(event) {
+        // prevent form submission
+        event.preventDefault();        
+        var $form = $('#bookDetailsForm');
+        
+        var inputValues = {};        
+        var ajaxURL = $form.attr('action') + $('.details-book-id').val();
+        $('.details-book-id').remove();
+        var ajaxData = $form.serializeArray();
+        var serialize = $form.serialize();        
+        var json = $form;
+
+        console.log($form);
+        console.log(ajaxData);
+        console.log(serialize);
+
+        console.log('ajaxUrl: ' + ajaxURL);        
+        var ajaxPut = $.ajax({ 
+            url: ajaxURL, 
+            type: 'PUT',
+            //headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
+            data: JSON.stringify(ajaxData),
+            contentType: "application/json; charset=utf-8",            
+        });
+        
+        ajaxPut.done(function( data ) {
+            $datatable.ajax.reload();
+
+            swal("Book saved!", "Book information has been updated.", "success");
+        });
+
+        ajaxPut.fail(function( jqXHR, textStatus, errorThrown ) {
+            swal("Save error.", "There was an error submitting the form.  " + errorThrown, "error");
+            console.log(jqXHR);
+            console.log(textStatus);
+
+        });    
+     });
+
+    $('#details-modal-delete-button').click(function(event) {
+        // prevent form submission
+        event.preventDefault();        
+        var $form = $('#bookDetailsForm');                
+        var ajaxURL = $form.attr('action') + $('.details-book-id').val();        
+               
+        var ajaxPut = $.ajax({ 
+            url: ajaxURL, 
+            type: 'DELETE',
+            //headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
+            //data: JSON.stringify(ajaxData),
+            contentType: "application/json; charset=utf-8",            
+        });
+        
+        ajaxPut.done(function( data ) {
+            $datatable.ajax.reload();
+
+            swal("Book deleted!", "Book has been deleted from the system.", "success");
+        });
+
+        ajaxPut.fail(function( jqXHR, textStatus, errorThrown ) {
+            swal("Delete error.", "There was an error submitting the form.  " + errorThrown, "error");
+            console.log(jqXHR);
+            console.log(textStatus);
+
+        });    
+     });  
 
     $('#add-author-button').click(function() { 
         AnimateSelector('#add-author-button', 'pulse');
@@ -171,40 +241,109 @@ $(document).ready( function () {
 //
 // Function Definitions
 //
-function UpdateFilterListBoxes() {        
-    $.getJSON("./assets/Library.json", function(json) {         
-        var data = json.data;
-        var authorList = ProcessAuthorsList(json.data);
-        var masterList = ProcessAllLists(json.data);  
-
-        // Append all unique authors from our list to the selectlist
-        for (i = 0; i < authorList.length; i++) {
-            $('.author-list').append('<option>' + authorList[i].toString() + '</option>');
-        }        
-
-        // Process & append other lists too
-        for (i = 0; i < masterList[0].length; i++) {
-            $('.core-value-list').append('<option>' + masterList[0][i].toString() + '</option>');
+// gets book details from API and loads to modal.
+function PopulateModal(clickedBookId) {
+    $('.details-control').hide();
+    var ajaxRequest = $.ajax({
+        url: "api/books/" + clickedBookId,
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data, textStatus, jqXHR) {
+            SetOutput($('.details-book-id'), clickedBookId);
+            SetOutput($('.details-title'), data[0].title);
+            SetOutput($('.details-author'), data[0].authors);            
+            SetOutput($('.details-status'), data[0].status);
+            SetOutput($('.details-reader'), data[0].currentreader);
+            SetOutput($('.details-core-value'), data[0].corevalue);            
+            $('#bookDetails').modal("show");
+        },
+        error: function(data, textStatus, jqXHR) {
+            console.log('error: ' + jqXHR);
+            return 'error';
         }
-        for (i = 0; i < masterList[1].length; i++) {
-            $('.current-reader-list').append('<option>' + masterList[1][i].toString() + '</option>');
+    });
+}
+
+function SetOutput ($object, textValue) {
+    $.each($object, function(index, item) {
+        if ($(item).is('input')) {
+            $(item).attr('value', textValue || 'None');
         }
-        for (i = 0; i < masterList[2].length; i++) {
-            $('.status-list').append('<option>' + masterList[2][i].toString() + '</option>');
-        }         
-    }).done(function() {
-        // use selectpicker a lot so after population, refresh ALL of them and hide the filters
-        $('select').addClass('selectpicker').attr('data-selected-text-format','count > 1');
-        $('.author-list').attr('data-selected-text-format', 'count > 2');
-        $('.selectpicker').selectpicker('refresh');
-        $('.filter').selectpicker('hide');
-    });     
+        else if ($(item).is('p')) {
+            $(item).text(textValue || 'None');
+        }
+        else if ($(item).is('select')) {
+            $(item).selectpicker('val', textValue);
+        }
+    });
+    
+    return $object;
+}
+
+// function for array.filter() method -- remove null, ""
+function TrimNullAndEmpty(obj) {
+    if (obj !== null && obj.id !== "" && obj.id !== null) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function UpdateFilterListBoxes(json) {    
+    var coreValueList = [];
+    var statusList = [];
+    var currentReaderList = [];
+    
+    ProcessAuthorsList();
+    // populate
+    for (var i = 0; i < json.length; i++) {
+        coreValueList.push(json[i].corevalue);
+        statusList.push(json[i].status);
+        currentReaderList.push(json[i].currentreader);
+    }
+    
+
+    // remove duplicates
+    var coreValueList = coreValueList.filter(function(itm,i,a){
+        return i==a.indexOf(itm);
+    });
+    var statusList = statusList.filter(function(itm,i,a){
+        return i==a.indexOf(itm);
+    });
+    var currentReaderList = currentReaderList.filter(function(itm,i,a){
+        return i==a.indexOf(itm);
+    });
+    
+    coreValueList = coreValueList.filter(TrimNullAndEmpty);
+    statusList= statusList.filter(TrimNullAndEmpty);
+    currentReaderList = currentReaderList.filter(TrimNullAndEmpty);
+    
+    // sort each specific list
+    coreValueList.sort();
+    statusList.sort();
+    currentReaderList.sort();
+
+    // append
+    for (var i = 0; i < coreValueList.length; i++) {
+        $('.core-value-list').append('<option>' + coreValueList[i] + '</option>');                        
+    }
+    for (var i = 0; i < statusList.length; i++) {
+        $('.status-list').append('<option>' + statusList[i] + '</option>');
+    }
+
+    for (var i = 0; i < currentReaderList.length; i++) {
+        $('.current-reader-list').append('<option>' + currentReaderList[i] + '</option>');
+    }
+
+
 };
 
 function FilterSearchDatatable($datatable, event, columnNumber) {
     // all selected <option> elements
     var selections = event.currentTarget.selectedOptions;
-    var selectionsArray = [].slice.call(selections);       
+    var selectionsArray = [].slice.call(selections);
 
     // add ( for regex search, use slice.call() to convert HTMLCollection to array for processing
     var searchString = '(';        
@@ -229,63 +368,29 @@ function FilterSearchDatatable($datatable, event, columnNumber) {
     }
 }
 
-function ProcessAllLists(jsonData) {
-    var coreValueList = [];
-    var statusList = [];
-    var currentReaderList = [];
-    var returnList = [];
-
-    for(var i=0; i < jsonData.length; i++) {           
-        coreValueList.push(jsonData[i].CoreValue.toString());
-        statusList.push(jsonData[i].Status.toString());
-        currentReaderList.push(jsonData[i].CurrentReader.toString());
-    }
-        
-    // remove duplicates
-    var coreValueList = coreValueList.filter(function(itm,i,a){
-        return i==a.indexOf(itm);
-    });
-    var statusList = statusList.filter(function(itm,i,a){
-        return i==a.indexOf(itm);
-    });
-    var currentReaderList = currentReaderList.filter(function(itm,i,a){
-        return i==a.indexOf(itm);
-    });
-    
-    // sort each specific list, compile and return
-    coreValueList.sort();
-    statusList.sort();
-    currentReaderList.sort();
-
-    returnList.push(coreValueList);
-    returnList.push(currentReaderList);
-    returnList.push(statusList);
-
-    return returnList;
-};
-
-function ProcessAuthorsList(jsonData) { 
-    var authorList = [];   
-    for(var i=0; i < jsonData.length; i++) {                
-        if (Array.isArray(jsonData[i].Authors)) {
-            for (var j=0; j < jsonData[i].Authors.length; j++) {
-                authorList.push(jsonData[i].Authors[j].toString());
+function ProcessAuthorsList() {    
+    var ajaxRequest = $.ajax({
+        url: "api/authors/",
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data, textStatus, jqXHR) {            
+            for (var i = 0; i < data.length; i++) {
+                $('.author-list').append('<option>' + data[i].author + '</option>');                
             }
+            
+            // use selectpicker a lot so after population, refresh ALL of them and hide the filters
+            $('select').addClass('selectpicker').attr('data-selected-text-format','count > 1');    
+            $('.author-list').attr('data-selected-text-format', 'count > 2');
+            $('.selectpicker').selectpicker('refresh');
+            $('.filter').selectpicker('hide');    
+        },
+        error: function(data, textStatus, jqXHR) {
+            console.log('error: ' + jqXHR);
+            return 'error';            
         }
-        else {
-            authorList.push(jsonData[i].Authors.toString());
-        }
-    }
-        
-    // remove duplicates
-    var uniqueAuthors = authorList.filter(function(itm,i,a){
-        return i==a.indexOf(itm);
-    });
-    
-    uniqueAuthors.sort();
-
-    return uniqueAuthors;
-};    
+    });    
+}
 
 function toggleFilter($elements) {
     $elements.slideToggle();
