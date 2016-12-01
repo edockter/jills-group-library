83fd00e5dafed820d8f9d0b53234f1c9b162a536
COMM-WORKS\edockter01 <Edockter01@comm-works.com>
2016-12-01 15:03:48 -0500

    whoops.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 var util = require('util');
-var pg = require('pg').native;
+var pg = require('pg');
 pg.defaults.ssl.true;
 const router = express.Router();
 var bodyParser = require('body-parser');
