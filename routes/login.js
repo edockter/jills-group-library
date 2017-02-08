5223abb7e5d1a339258846b62f5c692666efd083
Eric Dockter <ericdock@iupui.edu>
2017-02-08 02:56:14 -0500

    Build + send cookie if password is correct. Added /logout/ to delete cookie.

--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,8 @@ pg.defaults.ssl.true;
 const router = express.Router();
 var bodyParser = require('body-parser');
 var trim = require('trim');
+var cookie_name = process.env.COOKIE_NAME;
+var cookie_value = process.env.COOKIE_VALUE;
 
 // Replace with YOUR connection strings, or add to .env file.
 var connectionString;
@@ -25,11 +27,18 @@ router.post('/', (req, res, next) => {
     console.log(passwordSaved);
     
     if (passwordEntered.toUpperCase() === passwordSaved.toUpperCase()) {
-        res.status(200).send('success');
+        console.log("Sending cookie to ");
+        /* max age = 3 days in milliseconds */
+        res.cookie(cookie_name , cookie_value, { maxAge: 259200000 }).status(200).send('success');
     }
     else {
         res.status(400).send('password mismatch');
     }    
+});
+
+router.post('/logout', (req, res, next) => {
+    clearCookie(cookie_name);
+    res.status(200).send('Cookie deleted');
 });
 
 module.exports = router;
